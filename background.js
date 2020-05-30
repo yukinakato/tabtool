a71564e627daffd81176e5ed2f3f1214938dd404
Yuki Nakato <crystal.icicle@gmail.com>
2020-05-30 23:17:28 +0900

    expand window width

--- background.js
+++ background.js
@@ -14,14 +14,14 @@ function copyCb(tab) {
 
 function openTwitter(tab) {
   chrome.windows.getCurrent({ populate: false }, function (w) {
-    let pos_left = w.left + w.width - 540;
+    let pos_left = w.left + w.width - 640;
     let pos_top = w.top + w.height - tab[0].height;
     // let enc_title = encodeURIComponent(tab[0].title);
     // let enc_url = encodeURIComponent(tab[0].url);
     // let openurl = `https://twitter.com/intent/tweet?text=${enc_title}&url=${enc_url}`;
     let enc_text = encodeURIComponent("\n\n" + tab[0].title + "\n" + tab[0].url);
     let openurl = `https://twitter.com/intent/tweet?text=${enc_text}`;
-    let features = `width=540,height=360,left=${pos_left},top=${pos_top},scrollbars=yes,resizable=yes,toolbar=no,location=no`;
+    let features = `width=640,height=360,left=${pos_left},top=${pos_top},scrollbars=yes,resizable=yes,toolbar=no,location=no`;
     window.open(openurl, "_blank", features);
     tid = 0;
   });
